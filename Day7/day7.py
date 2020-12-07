7683d443740e7c2976d77f842169b1b2cf4971c8
Kyle <kylezhou2002@hotmail.com>
2020-12-07 01:56:26 -0800

    Add docstrings to helpers and class

--- Day7/day7.py
+++ Day7/day7.py
@@ -1,7 +1,13 @@
 class Bag:
     def __init__(self, colour):
-        """
-        bags is a list of tuples with bag and number
+        """A node representing a Bag and its contents
+
+        Args:
+            colour (str): The colour/description of the bag
+        
+        Instance Vars:
+            bags (list(tuple)): A list of tuples bag (Bag), quantity (int) that represents 
+                how much of the bag is contained within the current coloured Bag
         """
         self.colour = colour
         self.bags = []
@@ -13,15 +19,37 @@ class Bag:
         return f"{self.colour} bag containing {self.bags}"
 
     def add_bag(self, bag, quantity):
+        """Adds a required bag to the bag's contents
+
+        Args:
+            bag (Bag): The bag object to be added
+            quantity (int): The quantity of bag that must be in self
+        """
         self.bags.append((bag, quantity))
 
     def get_bag_count(self):
+        """Calculates and returns how many bags need to be in the current bag.
+
+        Returns:
+            int: The number of bags within the current bag
+        """
+        # b.get_bag_count() + 1 because get_bag_count does not count itself
+        # A bag does not contain itself for our purposes.
         return sum([(b.get_bag_count() + 1) * n for b, n in self.bags])
 
     def get_bag_type_count(self, colour):
+        """Recursively calculates and returns how many bags of a certain colour 
+            must be in the current bag.
+
+        Args:
+            colour (str): The bag's colour/description
+
+        Returns:
+            int: The number of bags with colour are contained in self
+        """
         count = 0
         for bag, quantity in self.bags:
-            # print(bag.colour, colour)
+            # This gets around the need to count oneself
             if bag.colour == colour:
                 count += quantity
             count += bag.get_bag_type_count(colour) * quantity
@@ -29,6 +57,17 @@ class Bag:
 
 
 def get_bag(bags, colour):
+    """Finds or creates a Bag with the requested colour/type
+       If the colour/bag does not exist in bags, a new Bag is 
+        created and added to bags
+
+    Args:
+        bags (dict(str: Bag)): A dict mapping colour to Bag objects
+        colour (str): Colour/type of bag requested
+
+    Returns:
+        Bag: The Bag with the requested colour contained within bags
+    """
     if colour in bags:
         bag = bags[colour]
     else:
