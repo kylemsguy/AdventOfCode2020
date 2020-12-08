5bea06b045bd03146e16d8a113618907c35b229f
Kyle Zhou <kylezhou2002@hotmail.com>
2020-12-08 00:09:59 -0800

    Cleanup Day 8 solution a bit

--- Day8/day8.py
+++ Day8/day8.py
@@ -1,5 +1,3 @@
-
-
 if __name__ == "__main__":
     with open("input.txt") as infile:
         raw = infile.read()
--- Day8/day8pt2.py
+++ Day8/day8pt2.py
@@ -1,13 +1,11 @@
-
 def execute(program):
     pc = 0
     acc = 0
     executed = []
     while pc not in executed:
         if pc >= len(program):
-            print("Boot sequence done")
+            print("Boot sequence complete")
             break
-        # print(pc, len(program))
         ins, arg = program[pc].split(' ')
         if ins == 'acc':
             acc += int(arg)
@@ -23,12 +21,14 @@ def execute(program):
 
     return pc, acc
 
+
 if __name__ == "__main__":
     with open("input.txt") as infile:
         raw = infile.read()
 
     lines = [line.strip() for line in raw.split('\n') if line.strip()]
 
+    # Try to simulate all possible patches
     for i, line in enumerate(lines):
         new_prog = lines[:]
         ins, arg = line.split(' ')
@@ -38,8 +38,10 @@ if __name__ == "__main__":
         elif ins == 'jmp':
             new_prog[i] = ' '.join(['nop', arg])
         else:
+            # Skip simulation because we patched nothing
             continue
         pc, acc = execute(new_prog)
-        if pc > 659:
-            print(i, line, acc)
+        if pc >= len(lines):
+            print("line(1-idx)\tinstruction\tacc when init complete")
+            print(i+1, line, acc, sep='\t\t')
             break
