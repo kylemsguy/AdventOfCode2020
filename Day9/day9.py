5c0310be91703d66e9908c4ba3bd58fa297c9ca5
Kyle <kylezhou2002@hotmail.com>
2020-12-08 21:27:36 -0800

    Cleanup Day 9

--- Day9/day9.py
+++ Day9/day9.py
@@ -4,17 +4,26 @@ if __name__ == "__main__":
 
     nums = [int(line.strip()) for line in raw.split('\n') if line.strip()]
 
-    for i in range(25, len(nums)-25):
+    # Search starting at the 26th number
+    for i in range(25, len(nums)):
         found = False
+        # Search the past 25 numbers
         for j in range(i-25, i):
-            for k in range(j+1, i+25):
+            # and add each of the next 24 numbers
+            for k in range(j+1, i):
+                # if we've found a sum that matches
                 if nums[i] == nums[j] + nums[k]:
+                    # Stop here, and mark as valid
                     found = True
                     break
+                # I wish there was a better way to jump out of nested loops...
                 if found:
                     break
+            # maybe a goto, like in C? Or maybe make a function and return...
             if found:
                 break
+        # We've found the number we were looking for
         if not found:
-            print("Not found", i, nums[i])
+            print("First number not meeting constraints:")
+            print(f"line {i+1}: {nums[i]}")
             break
--- Day9/day9pt2.py
+++ Day9/day9pt2.py
@@ -1,16 +1,28 @@
 if __name__ == "__main__":
+    print("Note: any line numbers are 1-indexed")
+    print()
     with open("input.txt") as infile:
         raw = infile.read()
 
     nums = [int(line.strip()) for line in raw.split('\n') if line.strip()]
 
+    # Copied from the output of day9.py
     target = 41682220
+    # Search the entire range, except for the last number (since seq length >= 2)
     for i in range(len(nums)-1):
         curr_range = []
         the_sum = nums[i]
+        # Add every number until the end of the file
         for j in range(i+1, len(nums)):
             the_sum += nums[j]
+            # Keep track of the sequence
             curr_range.append(nums[j])
+            # If we've found a matching sequence
             if the_sum == target:
-                print(min(curr_range)+max(curr_range))
+                print("We've found a matching sequence")
+                print(f"Starting line: {i+1}, ending line: {j+1}")
+                # Get the min and max of the sequence, and print the sum
+                print("Sum of min and max:", min(curr_range) + max(curr_range))
+
+                # I guess exit will do for jumping out of the nested loops
                 exit(0)
