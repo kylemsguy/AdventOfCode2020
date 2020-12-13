8bd1071858d4eed0dd3bb1229afd6f9b30f309c5
Kyle <kylezhou2002@hotmail.com>
2020-12-13 01:37:35 -0800

    update with WIP day13pt2

--- Day13/day13pt2.py
+++ Day13/day13pt2.py
@@ -2,10 +2,7 @@ import sys
 import math
 
 
-from functools import reduce
-
-def lcm(denominators):
-    return reduce(lambda a,b: a*b // math.gcd(a,b), denominators)
+import functools
 
 def earliest_bus(arrival, schedule):
     min_bus = None
@@ -86,9 +83,67 @@ def consecutive_bus3(schedule):
 
     return found_time
 
+def bezout_coeff(a, b):
+    old_r, r = a, b
+    old_s, s = 1, 0
+    old_t, t = 0, 1
+
+    while r != 0:
+        q = old_r // r
+        old_r, r = r, old_r - q * r
+        old_s, s = s, old_s - q * s
+        old_t, t = t, old_t - q * t
+
+    return old_s, old_t
+
 
 def consecutive_bus4(schedule):
-    pass
+    # This should be a list of tuples (a, n) such that x = a (mod n)
+    s = [(i, schedule[i]) for i in range(len(schedule)) if schedule[i]]
+    r_i = [x[0] for x in s]
+    m_i = [x[1] for x in s]
+    M = functools.reduce(lambda x, y: x * y, m_i)
+    a_i = [M // m for m in m_i]
+    i_i = [a % m for a, m in zip(a_i, m_i)]
+
+    Z = sum([i * r * a for i, r, a in zip(i_i, r_i, a_i)])
+
+    return Z % M
+
+def consecutive_bus5(schedule):
+    # This should be a list of tuples (a, n) such that x = a (mod n)
+    s = [(i, schedule[i]) for i in range(len(schedule)) if schedule[i]]
+    r_i = [x[0] for x in s] # remainders
+    m_i = [x[1] for x in s] # divisors
+
+    m1, m2 = m_i[:2]
+    r1, r2 = r_i[:2]
+
+    a, b = bezout_coeff(m1, m2)
+    result = a * m1 * r2 + b * m2 * r1 + (m1 * m2)
+
+    for i in range(2, len(s)):
+        m1 = m1 * m_i[i-1]
+        m2 = m_i[i]
+
+        r1 = result
+        r2 = r_i[i]
+
+        result = a * m1 * r2 + b * m2 * r1 + (m1 * m2)
+    
+    return result
+
+
+def consecutive_bus6(schedule):
+    # This should be a list of tuples (a, n) such that x = a (mod n)
+    s = [(i, schedule[i]) for i in range(len(schedule)) if schedule[i]]
+    r_i = [x[0] for x in s] # remainders
+    m_i = [x[1] for x in s] # divisors
+    M = functools.reduce(lambda x, y: x * y, m_i)
+    a_i = [M // m for m in m_i]
+    Z = sum([bezout_coeff(a_i[i], m_i[i])[0] * a_i[i] * a_i[i] for i in range(len(s))])
+
+    return Z % M
 
 
 if __name__ == "__main__":
@@ -108,4 +163,6 @@ if __name__ == "__main__":
     # print(b * w)
 
     # Part 2
-    print(consecutive_bus3(schedule))
+    print(consecutive_bus4(schedule))
+    print(consecutive_bus5(schedule))
+    print(consecutive_bus6(schedule))
